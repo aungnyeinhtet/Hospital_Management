79c476c0b8d8bb92d90961845b5ce2eaa5acc7a0
naysw <naysoewin9@gmail.com>
2022-10-01 00:08:57 +0630

    added more appointment test

--- __tests__/apointment.controller.e2e.spec.ts
+++ __tests__/apointment.controller.e2e.spec.ts
@@ -1,3 +1,4 @@
+import { ConsultationType } from "@prisma/client";
 import { genSaltSync, hash } from "bcrypt";
 import { Express } from "express";
 import request from "supertest";
@@ -17,8 +18,17 @@ describe("AppointmentController", () => {
     password: "password",
   };
 
+  const mockAppointment = {
+    consultationType: ConsultationType.VIDEO,
+    reason: "Sick",
+    from: new Date(Date.now()),
+    to: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+    doctorId: "6337142af55af0fe9c56610e",
+  };
+
   beforeAll(async () => {
     app = createApp();
+
     await prisma.patient.deleteMany();
 
     await prisma.patient.create({
@@ -46,7 +56,7 @@ describe("AppointmentController", () => {
     await prisma.$disconnect();
   });
 
-  describe("findMany", () => {
+  describe("findMany [GET]", () => {
     it("should throw Unauthorized error if user is not login", async () => {
       await request(app)
         .get(appointmentEnpoint)
@@ -64,6 +74,79 @@ describe("AppointmentController", () => {
 
       console.log(body);
       expect(body.data).toBeDefined();
+    });
+  });
+
+  describe("create [POST]", () => {
+    it("should throw Unauthorized error if user is not login", async () => {
+      await request(app)
+        .post(appointmentEnpoint)
+        .set("Accept", "application/json")
+        .set("Authorization", "")
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it("should throw error if required fileds are not provided", async () => {
+      await request(app)
+        .post(appointmentEnpoint)
+        .set("Accept", "application/json")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          consultationType: "",
+          reason: "",
+          from: "",
+          to: "",
+          doctorId: "",
+        })
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it("should throw error if consultationType is not valid enum", async () => {
+      await request(app)
+        .post(appointmentEnpoint)
+        .set("Accept", "application/json")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          consultationType: "Randon",
+          reason: mockAppointment.reason,
+          from: mockAppointment.from,
+          to: mockAppointment.to,
+          doctorId: mockAppointment.doctorId,
+        })
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it("should throw NotFound error if dotor not exists with doctorId", async () => {
+      const { body } = await request(app)
+        .post(appointmentEnpoint)
+        .set("Accept", "application/json")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send({
+          consultationType: mockAppointment.consultationType,
+          reason: mockAppointment.reason,
+          from: mockAppointment.from,
+          to: mockAppointment.to,
+          doctorId: "6337142af55af0fe9c56610b",
+        })
+        .expect(HttpStatus.NOT_FOUND);
+
+      console.log(body);
+    });
+
+    it.skip("should be create new appointment", async () => {
+      const { body } = await request(app)
+        .post(appointmentEnpoint)
+        .set("Accept", "application/json")
+        .set("Authorization", `Bearer ${accessToken}`)
+        .send(mockAppointment)
+        .expect(HttpStatus.CREATED);
+
+      console.log(body);
+
+      expect(body.data.consultationType).toBe(mockAppointment.consultationType);
+      expect(body.data.reason).toBe(mockAppointment.reason);
+      expect(body.data.from).toBe(mockAppointment.from);
+      expect(body.data.to).toBe(mockAppointment.to);
     });
   });
 });
