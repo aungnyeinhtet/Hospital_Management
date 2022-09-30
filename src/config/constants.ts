67a102fe93b2f4ad87ad230f3ea305dfc03f52f1
naysw <naysoewin9@gmail.com>
2022-09-30 19:52:04 +0630

    added token expires

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,3 +4,5 @@ export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 export const DEFAULT_TAKE = 20;
 export const MAX_SEE_PATIENT_PER_DAY = 20;
+
+export const ACCESS_TOKEN_EXPIRES_IN = "2h";
--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -2,12 +2,20 @@ import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { BadRequest } from "http-errors";
 import jwt from "jsonwebtoken";
+import { ACCESS_TOKEN_EXPIRES_IN } from "../config/constants";
 import { loginInputSchema } from "../dto/login.input";
 import { HttpStatus } from "../nsw/types/http-status";
 import * as patientService from "../services/patient.service";
 import { validate } from "../utils/validation";
 
-export const login = async (req: Request, res: Response) => {
+/**
+ * login
+ *
+ * @param req Request
+ * @param res Response
+ * @return Promise<void>
+ */
+export const login = async (req: Request, res: Response): Promise<void> => {
   const { phone, password } = await validate(req.body, loginInputSchema);
 
   const patient = await patientService.findByPhoneOrFail(phone);
@@ -16,15 +24,19 @@ export const login = async (req: Request, res: Response) => {
 
   if (!validatePassword) throw new BadRequest("Invalid password");
 
-  const token = jwt.sign(
+  const accessToken = jwt.sign(
     { sub: patient.id },
-    "process.env.ACCESS_TOKEN_SECRET",
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRES_IN,
+    },
   );
 
   res.status(HttpStatus.OK).json({
     data: {
-      token,
-      patient,
+      accessToken,
+      type: "Bearer",
+      expiredAt: ACCESS_TOKEN_EXPIRES_IN,
     },
   });
 };
